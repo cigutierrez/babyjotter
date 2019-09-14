e692a076c390e78ef885e8f524bdb3fd84ed13e7
AJ De La Costa <ade-la-costa@csumb.edu>
2019-09-14 09:56:04 -0700

    Created the migrations for babies and feedings

---
+++ database/migrations/2019_09_14_153720_create_babies.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBabies extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('babies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            // Needed for foreign key constraint for the users field
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->string('gender');
+            $table->timestamps();
+
+            // Foreign key constraint
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('babies');
+    }
+}
