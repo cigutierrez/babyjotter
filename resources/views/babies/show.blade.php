246a5ca86c95e04971570ea6f16eede0dc96fe76
Alfred De La Costa <ade-la-costa@csumb.edu>
2019-10-04 19:47:43 -0700

    CSS updated

--- resources/views/babies/show.blade.php
+++ resources/views/babies/show.blade.php
@@ -10,36 +10,49 @@
 
 <div class="row text-center justify-content-around mt-4" id="babyIconContainer">
     <!-- Feedings Button -->
-    <div class="col-2 feedingBtn">
+    <div class="col-md-2">
         <a href="">
-            <img src="/icons/feeding.png" alt="Feedings Icon" height=150 width=150>
+            <img src="/icons/feeding-icon.png" alt="Feedings Icon" class="babyBtns" height=150 width=150>
             <h4 class="pt-2">Feedings</h4>
         </a>
     </div>
     <!-- Diapers Button -->
-    <div class="col-2">
+    <div class="col-md-2">
         <a href="">
-            <img src="/icons/diaper.png" alt="Feedings Icon" height=150 width=150>
+            <img src="/icons/diaper.png" alt="Diapers Icon" class="babyBtns" height=150 width=150>
             <h4 class="pt-2">Diapers</h4>
         </a>
     </div>
     <!-- Sleep Button -->
-    <div class="col-2">
+    <div class="col-md-2">
         <a href="">
-            <img src="/icons/sleep.png" alt="Feedings Icon" height=150 width=150>
+            <img src="/icons/sleep.png" alt="Sleep Icon" class="babyBtns" height=150 width=150>
             <h4 class="pt-2">Naps</h4>
         </a>
     </div>
     <!-- Medication -->
-    <div class="col-2">
+    <div class="col-md-2">
         <a href="">
-            <img src="/icons/medication.png" alt="Feedings Icon" height=150 width=150>
+            <img src="/icons/medication.png" alt="Medication Icon" class="babyBtns" height=150 width=150>
             <h4 class="pt-2">Medications</h4>
         </a>
     </div>
 </div>
 
+<!-- Edit Baby Button -->
+<div class="row justify-content-center mt-3">
+    <div class="col-md-4">
+        <a href="/babies/{{ $baby->id }}/edit" class="btn btn-light btn-block">Edit Baby Details </a>
+    </div>
+</div>
 
-<!-- Insert navigation here -->
-@include('layouts.bottomNavigation')
+<!-- Back to babies button -->
+<div class="row justify-content-center mt-5">
+    <div class="col-md-6">
+        <a href="/babies" class="btn btn-light btn-block">Back to Babies</a>
+    </div>
+</div>
+
+
+
 @endsection
